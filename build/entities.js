49a536975fc2ac4dab88beb50600ddcf2bc33270
Ming Cheung <mingchun.cheung@hbo.com>
2020-08-12 11:24:14 -0700

    log test case title as attribute to report portal in runtime

--- build/entities.js
+++ build/entities.js
@@ -19,6 +19,21 @@ class StartTestItem {
             const attrs = tags.map((value) => (new ReporterOptions_1.Attribute("tag", value)));
             this.attributes.push(new ReporterOptions_1.Attribute("func", tags[0]));
             this.attributes.push(...attrs);
+            // if tags are detected, it's either the name of test feature, like:
+            // @tag1 @tag2 Test for a feature
+            //
+            // or the actual test scenario, like:
+            // @tag3, @tag4: test for a specific senceario
+            //
+            // on these 2 cases, we parse out the test name
+            // and send it over to report portal as attribute so each test feture / scenario
+            // can be uniquely identified via the attribute to create dashboard that
+            // can drilled down to individual test case level
+            const testName = this.name.match(/: (.+$)/);
+            if (testName && testName.length > 1) {
+                const title = testName[1].replace(/ /g, '-');
+                this.attributes.push(new ReporterOptions_1.Attribute("title", title));
+            }
         }
     }
 }
